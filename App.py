4991988e0ab41e1ea9738b86ffe5b51e440f36a4
guilhermegit <g360guilherme@gmail.com>
2024-02-26 02:23:31 -0300

    Tudo sussa

--- App.py
+++ App.py
@@ -2,14 +2,7 @@ import openpyxl
 woerkbook = openpyxl.load_workbook("produtos_ficticios.xlsx")
 sheet_produtos = woerkbook["Produtos"]
 
-
-
-
-           
-
-
-
-
-
+for linha in sheet_produtos.iter_rows(min_row=2):
+    print(linha[0].value)
 
 
